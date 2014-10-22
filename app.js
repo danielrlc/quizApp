561cfac12c51639af69911565e2080f4b726f558
thebluedaniel <daniel@Daniels-MacBook-Pro.local>
2014-10-22 22:04:27 +0300

    answer feedback and sound effects

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ function Question(q, a1, a2, a3, a4) {
 
 var q1 = new Question (
   "<audio src='q1.mp3' preload='auto' controls></audio>",
-  "<li>Mozart</li>",
-  "<li class='correct'>Shostakovich</li>",
-  "<li>Elgar</li>",
-  "<li>Bach</li>");
+  "<li class='wrong'>Mozart</li>",
+  "<li class='right'>Shostakovich</li>",
+  "<li class='wrong'>Elgar</li>",
+  "<li class='wrong'>Bach</li>");
 var q2 = new Question ("a1", "a2", "a3", "a4");
 var q3 = new Question ("a1", "a2", "a3", "a4");
 var q4 = new Question ("a1", "a2", "a3", "a4");
@@ -43,3 +43,17 @@ $(".question2").append(q2.a1, q2.a2, q2.a3, q2.a4);
 $(".question3").append(q3.a1, q3.a2, q3.a3, q3.a4);
 $(".question4").append(q4.a1, q4.a2, q4.a3, q4.a4);
 $(".question5").append(q5.a1, q5.a2, q5.a3, q5.a4);
+
+$(".right").one("click", function() {
+$(this).addClass("rightShow");
+$(this).append(" √ Good job!");
+$(this).append("<audio src='right.mp3' preload='auto' autoplay ></audio>");
+});
+
+$(".wrong").one("click", function() {
+$(this).addClass("wrongShow");
+$(this).append(" X Try again!");
+$(this).append("<audio src='wrong.mp3' preload='auto' autoplay ></audio>");
+});
+
+
