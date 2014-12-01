3cf589d1562c0565a15e6ee9d54e8245163c2338
thebluedaniel <daniel@Daniels-MacBook-Pro.local>
2014-12-01 20:30:15 +0200

    90% complete

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-// *** Can we go through all my comments and check that they show I understand what I'm doing?
+$( document ).ready(function() {
 
-// Declare global variables.
-var i = 1;
+// declare global variables
+var wrongCounter = 1;
 var score = 0;
-var qNum = 1;
+var qNum = 0;
 
-// Hide questions 2-5 in JavaScript, not in CSS, in case JavaScript is turned off.
-$(".q2, .q3, .q4, .q5").hide();
+// hide questions 2-5, so only one Q at a time
+$(".q1, .q2, .q3, .q4, .q5, .game-info").hide();
 
-// gray out previous and next navigation when they don't link anywhere
+
+// grey out previous and next navigation when they don't link anywhere
 var navGray = function() {
-  if (qNum === 1) {
+  if (qNum === 0) {
     $(".previous").addClass("previousGray");
   }
   else if (qNum === 5) {
@@ -20,31 +21,48 @@ var navGray = function() {
     $(".previous").removeClass("previousGray");
     $(".next").removeClass("nextGray");
   }
+  if (qNum !== 0) {
+    $(".game-info").show();
+  }
 };
 
 navGray();
 
-// Only one question showing at a time. Player clicks "next" button to navigate through questions.
+// Pause audio when user clicks anywhere on page.
+// Thanks for help here:
+// http://stackoverflow.com/questions/965601
+$(document).click(function(){
+     $('audio').each(function() {
+     if(!this.paused){
+        this.pause();
+     }
+    });
+});
+
+// Only one question showing at a time.
+// Player clicks "next" button to navigate through questions.
 $(".next").on("click", function() {
-  // *** set i globally, but is this a bad solution normally?
-  i = 1;
+  // set wrongCounter, so can give different feedback
+  // for each wrong answer
+  wrongCounter = 1;
   $(".feedback").empty();
   $(".qNum").empty();
   if (qNum !== 5) {
     qNum++;
   }
   navGray();
-  $(".qNum").append("Question " + qNum + " of 5");
+  $(".qNum").append("Question: " + qNum + " of 5");
   $(".q" + qNum).show();
   $(".q" + (qNum - 1)).hide();
 });
 
 $(".previous").on("click", function() {
-  // *** set i globally, but is this a bad solution normally?
-  i = 1;
+  // set wrongCounter, so can give different feedback
+  // for each wrong answer
+  wrongCounter = 1;
   $(".feedback").empty();
   $(".qNum").empty();
-  if (qNum !== 1) {
+  if (qNum !== 0) {
     qNum--;
   }
   navGray();
@@ -53,8 +71,54 @@ $(".previous").on("click", function() {
   $(".q" + (qNum + 1)).hide();
 });
 
+// ***************************************************************************************************************************
+
+// Feedback if player chooses right answer.
+$(".right").one("click", function() {
+  $(".feedback").empty();
+  $(".feedback").append("Correct!");
+  $(this).addClass("rightShow");
+  $(this).append("<audio src='tada-right.mp3' preload='auto' autoplay volume=.2></audio>");
+  if (wrongCounter === 1) {
+    score += 20;
+  }
+  $(".score").empty();
+  $(".score").append("Score: " + score + "%");
+  
+  // This makes sure player can't pick a wrong answer after selecting the right answer.
+  wrongCounter += 4;
+
+  // $(".audio-track").pause();
+
+});
+
+// Feedback if player chooses wrong answer. Different feedback is given for first, second and third wrong answer.
+$(".wrong").one("click", function() {
+  if (wrongCounter < 4) {
+  $(this).addClass("wrongShow");
+  $(this).append("<audio src='gongWrong.mp3' preload='auto' autoplay ></audio>");
+  }
+  else {
+  }
+  if (wrongCounter === 1) {
+    $(".feedback").empty();
+    $(".feedback").append("You have gong wrong.");
+  }
+  else if (wrongCounter === 2) {
+    $(".feedback").empty();
+    $(".feedback").append("Still wrong, thus the gong.");
+  }
+  else if (wrongCounter === 3) {
+    $(".feedback").empty();
+    $(".feedback").append("Maybe you just like the sound of gongs?");
+  }
+  wrongCounter++;
+});
+
+// ***************************************************************************************************************************
+
 // Create prototype object called Question. All question objects will be constructed from this blueprint. 
-function Question(qText, qAudio, a1, a2, a3, a4, aInfo) {
+var Question = function(qText, qAudio, a1, a2, a3, a4, aInfo) {
     this.qText = qText;
     this.qAudio = qAudio;
     this.a1 = a1;
@@ -62,107 +126,71 @@ function Question(qText, qAudio, a1, a2, a3, a4, aInfo) {
     this.a3 = a3;
     this.a4 = a4;
     this.aInfo = aInfo;
-}
+};
 
 // Create question 1 object from Question blueprint.
 var q1 = new Question (
-  "<p>You're about to hear the start of a popular symphony. But who wrote it?</p>",
-  "<p><audio src='q1.mp3' preload='auto' controls></audio></p>",
-  "<li class='wrong'>Mozart</li>",
-  "<li class='right'>Shostakovich</li>",
-  "<li class='wrong'>Elgar</li>",
-  "<li class='wrong'>Bach</li>",
-  "<p>Shostakovich wrote this symphony in 1939-1940 during the siege of his hometown Leningrad.</p>"
+  "<p></p>",
+  "<p><audio src='raiders.mp3' preload='auto' controls></audio></p>",
+  "<li class='wrong'>Star Wars</li>",
+  "<li class='right'>Raiders of the Lost Ark</li>",
+  "<li class='wrong'>Jurassic Park</li>",
+  "<li class='wrong'>Superman</li>",
+  "<p></p>"
   );
 
 // Create question 2 object from Question blueprint.
 var q2 = new Question (
-  "<p>You're about to hear the start of a popular symphony. But who wrote it?</p>",
-  "<p><audio src='q1.mp3' preload='auto' controls></audio></p>",
-  "<li class='wrong'>Mozart</li>",
-  "<li class='right'>Shostakovich</li>",
-  "<li class='wrong'>Elgar</li>",
-  "<li class='wrong'>Bach</li>",
-  "<p>Shostakovich wrote this symphony in 1939-1940 during the siege of his hometown Leningrad.</p>"
+  "<p></p>",
+  "<p><audio src='seven-years.mp3' preload='auto' controls></audio></p>",
+  "<li class='wrong'>Hook</li>",
+  "<li class='wrong'>Saving Private Ryan</li>",
+  "<li class='wrong'>Schindler's List</li>",
+  "<li class='right'>Seven Years in Tibet</li>",
+  "<p></p>"
   );
 
 // Create question 3 object from Question blueprint.
 var q3 = new Question (
-  "<p>You're about to hear the start of a popular symphony. But who wrote it?</p>",
-  "<p><audio src='q1.mp3' preload='auto' controls></audio></p>",
-  "<li class='wrong'>Mozart</li>",
-  "<li class='right'>Shostakovich</li>",
-  "<li class='wrong'>Elgar</li>",
-  "<li class='wrong'>Bach</li>",
-  "<p>Shostakovich wrote this symphony in 1939-1940 during the siege of his hometown Leningrad.</p>"
+  "<p></p>",
+  "<p><audio src='jurassic-park.mp3' preload='auto' controls></audio></p>",
+  "<li class='right'>Jurassic Park</li>",
+  "<li class='wrong'>Hook</li>",
+  "<li class='wrong'>The Temple of Doom</li>",
+  "<li class='wrong'>Home Alone</li>",
+  "<p></p>"
   );
 
 // Create question 4 object from Question blueprint.
 var q4 = new Question (
-  "<p>You're about to hear the start of a popular symphony. But who wrote it?</p>",
-  "<p><audio src='q1.mp3' preload='auto' controls></audio></p>",
-  "<li class='wrong'>Mozart</li>",
-  "<li class='right'>Shostakovich</li>",
-  "<li class='wrong'>Elgar</li>",
-  "<li class='wrong'>Bach</li>",
-  "<p>Shostakovich wrote this symphony in 1939-1940 during the siege of his hometown Leningrad.</p>"
+  "<p></p>",
+  "<p><audio src='schindlers-list.mp3' preload='auto' controls></audio></p>",
+  "<li class='wrong'>Hook</li>",
+  "<li class='right'>Schindler's List</li>",
+  "<li class='wrong'>Far and Away</li>",
+  "<li class='wrong'>The Prisoner of Azkaban</li>",
+  "<p></p>"
   );
 
 // Create question 5 object from Question blueprint.
 var q5 = new Question (
-  "<p>You're about to hear the start of a popular symphony. But who wrote it?</p>",
-  "<p><audio src='q1.mp3' preload='auto' controls></audio></p>",
-  "<li class='wrong'>Mozart</li>",
-  "<li class='right'>Shostakovich</li>",
-  "<li class='wrong'>Elgar</li>",
-  "<li class='wrong'>Bach</li>",
-  "<p>Shostakovich wrote this symphony in 1939-1940 during the siege of his hometown Leningrad.</p>"
+  "<p></p>",
+  "<p><audio src='superman.mp3' preload='auto' controls></audio></p>",
+  "<li class='wrong'>Saving Private Ryan</li>",
+  "<li class='wrong'>Rosewood</li>",
+  "<li class='right'>Superman</li>",
+  "<li class='wrong'>The Temple of Doom</li>",
+  "<p></p>"
   );
 
 // Paste question object properties into CSS containers.
-$(".q1").append(q1.qText, q1.qAudio, q1.a1, q1.a2, q1.a3, q1.a4);
-$(".q2").append(q2.qText, q2.qAudio, q2.a1, q2.a2, q2.a3, q2.a4);
-$(".q3").append(q3.qText, q3.qAudio, q3.a1, q3.a2, q3.a3, q3.a4);
-$(".q4").append(q4.qText, q4.qAudio, q4.a1, q4.a2, q4.a3, q4.a4);
-$(".q5").append(q5.qText, q5.qAudio, q5.a1, q5.a2, q5.a3, q5.a4);
+$(".q0").append("<p>Hello there!</p><p>John Williams has written many of the greatest film scores of the past 40 years. This quiz is a small tribute to his musical talents. So can you match the music to the film?</p><p>Sit back, get your musical ears ready, and turn the volume right up.</p>")
+$(".q1").append(q1.qAudio, q1.a1, q1.a2, q1.a3, q1.a4);
+$(".q2").append(q2.qAudio, q2.a1, q2.a2, q2.a3, q2.a4);
+$(".q3").append(q3.qAudio, q3.a1, q3.a2, q3.a3, q3.a4);
+$(".q4").append(q4.qAudio, q4.a1, q4.a2, q4.a3, q4.a4);
+$(".q5").append(q5.qAudio, q5.a1, q5.a2, q5.a3, q5.a4);
 
-// Feedback if player chooses right answer.
-$(".right").one("click", function() {
-  $(".feedback").empty();
-  $(".feedback").append("Good job!");
-  $(this).addClass("rightShow");
-  $(this).append("<audio src='taDaRight.mp3' preload='auto' autoplay ></audio>");
-  $(".feedback").append(q1.aInfo);
-  if (i === 1) {
-    score += 20;
-  }
-  $(".score").empty();
-  $(".score").append("Your score is " + score + "%");
-  
-  // *** This is a bit of a fudge. It's to make sure player can't pick a wrong answer after selecting the right answer. Any better way to do this?
-  i += 4;
-});
-
-// Feedback if player chooses wrong answer. Different feedback is given for first, second and third wrong answer.
-$(".wrong").one("click", function() {
-  if (i < 4) {
-  $(this).addClass("wrongShow");
-  $(this).append("<audio src='gongWrong.mp3' preload='auto' autoplay ></audio>");
-  }
-  else {
-  }
-  if (i === 1) {
-    $(".feedback").append("You have gong wrong.");
-  }
-  else if (i === 2) {
-    $(".feedback").empty();
-    $(".feedback").append("Still wrong, thus the gong.");
-  }
-  else if (i === 3) {
-    $(".feedback").empty();
-    $(".feedback").append("Maybe you just like the sound of gongs?");
-  }
-  i++;
 });
 
 
